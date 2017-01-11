2ebe2a1871c9f0d92d03bc403ba172453b2e765b
Nicolai <nicolai@mmg.io>
2017-01-11 12:30:39 +0000

    Objects learning tiiiiiiime

---
+++ objectivelybadprogramming.js
@@ -0,0 +1,15 @@
+var person = {
+  name : 'Sarah',
+  country : 'US',
+  age : 35,
+  treehouseStudent : true,
+  skills : ['JavaScript', 'HTML', 'CSS']
+};
+
+function print(message) {
+  var div = document.getElementById('output');
+  div.innerHTML = message;
+}
+
+var message = '<p> Hello. My name is ' + person.name + '</p>';
+print(message);
